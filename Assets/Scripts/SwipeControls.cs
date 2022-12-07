d21eebc38917aa0cdfc2b86883af2d116ac88bf4
Gutu_Nicolae <nicolausclauss@gmail.com>
2022-12-07 17:33:24 +0200

    Added SwipeControls

---
+++ Assets/Scripts/SwipeControls.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeControls : MonoBehaviour
+{
+    public enum SwipeDirection
+    {
+        Up, Down, Left, Right
+    }
+
+    public static event System.Action<SwipeDirection> OnSwipe = delegate { };
+
+    Vector2 swipeStart;
+    Vector2 swipeEnd;
+
+    float minDistance = 10;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                swipeStart = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                swipeEnd = touch.position;
+                ProcessSwipe();
+            }
+        }
+
+        //mouse touch simulation
+        if (Input.GetMouseButtonDown(0))
+        {
+            swipeStart = Input.mousePosition;
+        }
+        else
+        {
+            swipeEnd = Input.mousePosition;
+            ProcessSwipe();
+        }
+
+    }
+
+    void ProcessSwipe()
+    {
+        float distance = Vector2.Distance(swipeStart, swipeEnd);
+
+        if (distance > minDistance)
+        {
+            if (IsVerticalSwipe())
+            {
+                if (swipeEnd.y > swipeStart.y)
+                {
+                    OnSwipe(SwipeDirection.Up);
+                }
+                else
+                {
+                    OnSwipe(SwipeDirection.Down);
+                }
+            }
+            else //IsHorizontalSwipe
+            {
+                if (swipeEnd.x > swipeStart.x)
+                {
+                    OnSwipe(SwipeDirection.Right);
+                }
+                else
+                {
+                    OnSwipe(SwipeDirection.Left);
+                }
+            }
+        }
+    }
+
+    bool IsVerticalSwipe()
+    {
+        float vertical = Mathf.Abs(swipeEnd.y - swipeStart.y);
+        float horizontal = Mathf.Abs(swipeEnd.x - swipeStart.x);
+
+        if (vertical > horizontal)
+            return true;
+        return false;
+    }
+}
