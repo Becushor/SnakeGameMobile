843a2a5f40eebcb5412eaf18f28292cb30cca298
Gutu_Nicolae <nicolausclauss@gmail.com>
2022-12-11 17:19:43 +0200

    Added Walls

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,12 @@ public class GameController : MonoBehaviour
 {
     public static GameController instance = null;
 
+    const float Width = 3.7f;
+    const float Height = 7f;
+
     public float snakeSpeed = 1;
+
+    public GameObject rockPrefab = null;
 
     public BodyPart bodyPrefab = null;
     public SnakeHead snakeHead = null;
@@ -17,6 +22,7 @@ public class GameController : MonoBehaviour
     void Start()
     {
         instance = this;
+        CreateWalls();
         StartGame();
     }
 
@@ -28,5 +34,47 @@ public class GameController : MonoBehaviour
     void StartGame()
     {
         snakeHead.ResetSnake();
+    }
+
+    void CreateWalls()
+    {
+        Vector3 start = new Vector3(-Width, -Height, 0);
+        Vector3 finish = new Vector3(-Width, Height, 0);
+        CreateWall(start, finish);
+        
+        start = new Vector3(Width, -Height, 0);
+        finish = new Vector3(Width, Height, 0);
+        CreateWall(start, finish);
+
+        start = new Vector3(-Width, -Height, 0);
+        finish = new Vector3(Width, -Height, 0);
+        CreateWall(start, finish);
+
+        start = new Vector3(-Width, Height, 0);
+        finish = new Vector3(Width, Height, 0);
+        CreateWall(start, finish);
+    }
+
+    void CreateWall(Vector3 start, Vector3 finish)
+    {
+        float distance = Vector3.Distance(start, finish);
+        int numberOfRocks = (int)(distance * 3f);
+        Vector3 delta = (finish - start) / numberOfRocks;
+
+        Vector3 position = start;
+
+        for (int i = 0; i <= numberOfRocks; i++)
+        {
+            float scale = Random.Range(1.5f, 2f);
+            float rotation = Random.Range(0, 360f);
+            CreateRock(position, scale, rotation);
+            position += delta;
+        }
+    }
+
+    void CreateRock(Vector3 position, float scale, float rotation)
+    {
+        GameObject rock = Instantiate(rockPrefab, position, Quaternion.Euler(0, 0, rotation));
+        rock.transform.localScale = new Vector3(scale, scale, 1);
     }
 }
